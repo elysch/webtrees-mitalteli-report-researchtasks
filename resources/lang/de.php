df862320329b6c30265070808dcac245d467e6ea
Holger Eisold <github@eisold.family>
2026-01-20 00:26:29 +0100

    added german translation
    Signed-off-by: Holger Eisold <github@eisold.family>

---
+++ resources/lang/de.php
@@ -0,0 +1,50 @@
+<?php
+
+    return [
+
+    'A research tasks Report with filters and other options.' => 'Ein Forschungsaufgabenbericht mit Filtern und anderen Optionen.', // mitalteli-report-researchtasks/app/ResearchTasksReportModule.php
+    'Individual' => 'Person', // mitalteli-report-researchtasks/app/ResearchTasksReportModule.php
+    'Report' => 'Report', // Http/RequestHandlers/MitalteliReportGenerate.php
+    'Private' => 'Vertraulich', // app/Report/MitalteliReportParserGenerate.php
+    '…' => '…', // app/Report/MitalteliReportParserGenerate.php
+    'Research tasks report' => 'Bericht über Forschungsaufgaben', // mitalteli-report-researchtasks/app/ResearchTasksReportModule.php
+    'Research tasks report with filters and other options' => 'Bericht zu Forschungsaufgaben mit Filtern und anderen Optionen', // mitalteli-report-researchtasks/resources/report.xml
+    'Show parents' => 'Eltern anzeigen', // mitalteli-report-researchtasks/resources/report.xml
+    'Show XREF' => 'XREF anzeigen', // mitalteli-report-researchtasks/resources/report.xml
+    'Show Filters' => 'Filter anzeigen', // mitalteli-report-researchtasks/resources/report.xml
+    'Hide records without a date' => 'Datensätze ohne Datum ausblenden', // mitalteli-report-researchtasks/resources/report.xml
+    'Include all names on individuals' => 'Alle Namen der Personen einschließen', // mitalteli-report-researchtasks/resources/report.xml
+    'Show all individuals' => 'Alle Personen anzeigen', // mitalteli-report-researchtasks/resources/report.xml
+    'Show all families' => 'Alle Familien anzeigen', // mitalteli-report-researchtasks/resources/report.xml
+    'Research Task Filter' => 'Forschungsaufgabenfilter', // mitalteli-report-researchtasks/resources/report.xml
+    'Match whole string' => 'Ganze Zeichenfolge abgleichen', // mitalteli-report-researchtasks/resources/report.xml
+    'Match ignoring case' => 'Groß- und Kleinschreibung ignorieren', // mitalteli-report-researchtasks/resources/report.xml
+    'Match ignoring diacritics' => 'Diakritische Zeichen ignorieren', // mitalteli-report-researchtasks/resources/report.xml
+    'Research task creation date range start' => 'Forschungsaufgabe Datumsbereich Beginn', // mitalteli-report-researchtasks/resources/report.xml
+    'Research task creation date range end' => 'Forschungsaufgabe Datumsbereich Ende', // mitalteli-report-researchtasks/resources/report.xml
+    'sort by lastname' => 'Nach Nachname sortieren', // mitalteli-report-researchtasks/resources/report.xml
+    'sort date (only takes first research task)' => 'Sortieren nach Datum (beachtet nur erste Rechercheaufgabe)', // mitalteli-report-researchtasks/resources/report.xml
+    'sort by text (only takes first research task)' => 'Sortieren nach Text (beachtet nur erste Rechercheaufgabe)', // mitalteli-report-researchtasks/resources/report.xml
+    'sort by note (only takes first research task)' => 'Sortieren nach Notiz (beachtet nur erste Rechercheaufgabe)', // mitalteli-report-researchtasks/resources/report.xml
+    'sort by user (only takes first research task)' => 'Sortieren nach Benutzer (beachtet nur erste Rechercheaufgabe)', // mitalteli-report-researchtasks/resources/report.xml
+    'Sort order' => 'Sortierreihenfolge', // mitalteli-report-researchtasks/resources/report.xml
+    'Page size' => 'Seitengröße', // mitalteli-report-researchtasks/resources/report.xml
+    'Page' => 'Seite', // mitalteli-report-researchtasks/resources/report.xml
+    'of' => 'von', // mitalteli-report-researchtasks/resources/report.xml
+    'Individuals' => 'Persnen', // mitalteli-report-researchtasks/resources/report.xml
+    'XREF' => 'XREF', // mitalteli-report-researchtasks/resources/report.xml
+    'Name' => 'Name', // mitalteli-report-researchtasks/resources/report.xml
+    'Research Task' => 'Forschungsaufgabe', // mitalteli-report-researchtasks/resources/report.xml
+    'Note' => 'Notiz', // mitalteli-report-researchtasks/resources/report.xml
+    'Date' => 'Datum', // mitalteli-report-researchtasks/resources/report.xml
+    'User' => 'Benutzer', // mitalteli-report-researchtasks/resources/report.xml
+    'Father' => 'Vater', // mitalteli-report-researchtasks/resources/report.xml
+    'Mother' => 'Mutter', // mitalteli-report-researchtasks/resources/report.xml
+    'Total individuals' => 'Gesamtzahl der Personen', // mitalteli-report-researchtasks/resources/report.xml
+    'Families' => 'Familien', // mitalteli-report-researchtasks/resources/report.xml
+    'Total families' => 'Gesamtzahl der Familien', // mitalteli-report-researchtasks/resources/report.xml
+    'Report Parameters' => 'Berichtsparameter', // mitalteli-report-researchtasks/resources/report.xml
+    'About sort: It takes the first fact of each individual or family to sort, ignores the rest and sends empty values to the end.' => 'Informationen zum Sortieren: Zum Sortieren wird der erste Fakt jeder Person oder jeder Familie verwendet, der Rest wird ignoriert und leere Werte werden an das Ende übertragen.', // mitalteli-report-researchtasks/resources/report.xml
+
+    ];
+?>
